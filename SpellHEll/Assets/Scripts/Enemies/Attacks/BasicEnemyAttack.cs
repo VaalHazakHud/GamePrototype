9a77b9f017f09affa644f213c20ae1f0c788b9b2
Oli H <58710381+u1969310@users.noreply.github.com>
2020-03-24 20:23:51 +0000

    Enemy AI
    Added a BasicEnemyAI script to aim/ chase etc.
    Added a StrafingBEAI script, child of BEAI
    Added CircilingEnemyBEAI, child of BEAI (Does not function as intended.)

---
+++ SpellHEll/Assets/Scripts/Enemies/Attacks/BasicEnemyAttack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyAttack : MonoBehaviour{
+    
+    public float projectileSpeed;    
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;    
+    private Vector2 target;
+    protected Rigidbody2D rb;
+    protected Transform player;
+
+    // Start is called before the first frame update
+    void Start(){
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = this.GetComponent<Rigidbody2D>();
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        //BasicFunctions();                    
+
+        if (timeBtwShots <= 0) {
+            Shoot();
+            timeBtwShots = startTimeBtwShots;             
+        }
+
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }        
+    }              
+
+    void Shoot() {
+        GameObject Bullet = Instantiate(projectile, transform.position + transform.up, Quaternion.identity);
+        Debug.Log(transform.rotation);
+        Rigidbody2D rb = Bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * projectileSpeed, ForceMode2D.Impulse);
+    }
+}
---
+++ SpellHEll/Assets/Scripts/Enemies/BasicEnemyAI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyAI : MonoBehaviour { 
+
+
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+    protected Rigidbody2D rb;
+    
+
+    protected Transform player;
+    private Vector3 startingPosition;
+    
+
+    // Start is called before the first frame update
+    private void Start()    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update ()    {
+
+        BasicFunctions();   
+    }
+
+    public void BasicFunctions() {
+
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        else if (Vector2.Distance(transform.position, player.position) <
+            stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance) {
+            transform.position = this.transform.position;
+        }
+
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        Vector2 lookDir = player.GetComponent<Rigidbody2D>().position - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+    }
+
+}    
