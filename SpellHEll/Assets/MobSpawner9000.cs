f34bd68281e3b7d21348c22fcdae56ce38ca58ee
Brevan4 <Brevan4@users.noreply.github.com>
2020-03-26 03:59:37 +0000

    HasDespawner

---
+++ SpellHEll/Assets/MobSpawner9000.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner9000 : MonoBehaviour
+{
+    private void Start()
+    {
+
+        StartCoroutine("SpawnTimer");
+    }
+    public GameObject Monster;
+    IEnumerator SpawnTimer()
+    {
+        for (; ; )
+        {
+            Spawn();
+            yield return new WaitForSeconds(10.5f);
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject monster = Instantiate(Monster, transform.position, Quaternion.identity);
+    }
+
+}
---
+++ SpellHEll/Assets/Scripts/Detector.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+
+    public bool IsHostiles;
+    public GameObject[] gameObjects;
+    public GameObject[] Pause;
+
+
+    void Start()
+    {
+        Pause = GameObject.FindGameObjectsWithTag("Loot");
+
+    }
+
+
+
+
+    void Update()
+    {
+        ProximityCheck();
+        if (IsHostiles == false) TurnOff();
+        if (IsHostiles == true) TurnOn();
+        
+    
+    }
+
+    void ProximityCheck()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Baddie");
+        if (gameObjects.Length != 0)
+        {
+            IsHostiles = true;
+        }
+        else
+        {
+            IsHostiles = false;
+        }        
+        
+    }
+
+    void TurnOff()
+    {
+       foreach(GameObject i in Pause)
+        {
+            i.SetActive(false);
+        }
+    }
+    void TurnOn()
+    {
+        foreach (GameObject i in Pause)
+        {
+            i.SetActive(true);
+        }
+    }
+    
+
+}
