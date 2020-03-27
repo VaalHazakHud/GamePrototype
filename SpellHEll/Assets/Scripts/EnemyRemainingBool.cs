dc4fcd705ce48b6515692e4b31fc01254f4e4830
Oli H <58710381+u1969310@users.noreply.github.com>
2020-03-27 15:41:41 +0000

    Enemy Types
    - Added 2 Enemy Prefabs (Strafer, Tower)
      (Neither come with different Sprites or with a projectile object.)

---
+++ SpellHEll/Assets/Scripts/EnemyRemainingBool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRemainingBool : MonoBehaviour {
+
+
+    public GameObject Portal;
+    public GameObject Player;    
+    public bool mobs;
+    GameObject[] enemies;
+    
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        enemies = GameObject.FindGameObjectsWithTag("Baddie");
+
+        if (enemies.Length != 0) {
+            mobs = true;
+            
+        }
+
+        else {
+            mobs = false;            
+        }
+
+    }
+    
+    public void OnTriggerStay2D(Collider2D other) {
+        
+
+        if (enemies.Length == 0 && other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E)) {
+
+            Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+        }
+    }
+
+    }
+
+
