df26b33933ec70ea52204702c3749b75e3e35348
Brevan4 <Brevan4@users.noreply.github.com>
2020-03-30 20:39:05 +0100

    Has Spawner

---
+++ SpellHEll/Assets/MobSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public GameObject Monster;
+   
+
+
+    public void Spawn()
+    {
+        GameObject monster = Instantiate(Monster, transform.position, Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+ 
+}
---
+++ SpellHEll/Assets/Mobspawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobspawn : MonoBehaviour
+{
+    public bool triggerOn;
+    public bool spawnShoot;
+    
+
+
+    void Start()
+    {
+        triggerOn = true;
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && triggerOn == true)
+        {
+            foreach(Transform child in gameObject.transform)
+            {
+                child.gameObject.GetComponent<MobSpawner>().Spawn();
+            }
+            triggerOn = false;
+                       
+        }
+    }
+}
